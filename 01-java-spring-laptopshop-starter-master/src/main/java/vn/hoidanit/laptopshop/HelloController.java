91b216bfbdb98fdfd50be29d0c15cf139b135ac6
Nguyen Van Cuwr <nguyencu14503@gmail.com>
2025-09-25 16:30:16 +0700

    capnhatt

--- 01-java-spring-laptopshop-starter-master/src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ 01-java-spring-laptopshop-starter-master/src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "Hello World with chay ngay di!";
+        return "Hello World with cham lai thoi!";
     }
 
 }
